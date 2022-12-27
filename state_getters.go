4beb8468ab6e03f5e9d148f551e430ef1bddac6e
Mingye Chen <mingyechen82@gmail.com>
2022-12-26 20:45:20 -0500

    Add doc

--- state_getters.go
+++ state_getters.go
@@ -2,6 +2,7 @@ package dtls
 
 import "github.com/mingyech/dtls/v2/pkg/protocol/handshake"
 
+// RemoteRandomBytes returns the random bytes from the client or server hello
 func (s *State) RemoteRandomBytes() [handshake.RandomBytesLength]byte {
 	return s.remoteRandom.RandomBytes
 }
